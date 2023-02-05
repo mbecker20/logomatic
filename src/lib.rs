9850b07ccc43a86bc053e6863dd7f06a0a5da993
mbecker20 <becker.maxh@gmail.com>
2023-02-05 06:53:32 +0000

    functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    fmt::Display,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use serde::Serialize;
+use serde_derive::Serialize;
+
+pub use serde_json::json;
+
+#[derive(Serialize)]
+pub struct LogMessage<T: Serialize, D: Serialize> {
+    pub ts: i64,
+    #[serde(rename = "type")]
+    pub log_type: T,
+    pub data: D,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn create_log_msg<T: Serialize, D: Serialize>(log_type: T, data: D) -> LogMessage<T, D> {
+    LogMessage {
+        ts: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as i64,
+        log_type,
+        data,
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn log(log_type: impl Serialize, data: impl Serialize) {
+    println!("{}", create_log_msg(log_type, data));
+}
+
+pub fn log_err(log_type: impl Serialize, data: impl Serialize) {
+    eprintln!("{}", create_log_msg(log_type, data));
+}
+
+pub fn log_json(log_type: impl Serialize, data: impl Serialize) {
+    println!(
+        "{}",
+        serde_json::to_string(&create_log_msg(log_type, data)).unwrap()
+    );
+}
+
+pub fn log_err_json(log_type: impl Serialize, data: impl Serialize) {
+    eprintln!(
+        "{}",
+        serde_json::to_string(&create_log_msg(log_type, data)).unwrap()
+    );
+}
+
+impl<T: Serialize, D: Serialize> Display for LogMessage<T, D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} | {} | {}",
+            self.ts,
+            serde_json::to_string(&self.log_type).unwrap(),
+            serde_json::to_string(&self.data).unwrap()
+        )
     }
 }
